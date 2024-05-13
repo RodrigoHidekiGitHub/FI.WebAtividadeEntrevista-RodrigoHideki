d3e56d48cbd37edc37925de683fab542004b3aa0
Rodrigo Hideki <rodrigo.hidex@gmail.com>
2024-05-13 05:06:42 -0300

    Add project files.

---
+++ FI.WebAtividadeEntrevista/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,9 @@
+namespace WebAtividadeEntrevista.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
+
+
---
+++ FI.WebAtividadeEntrevista/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebAtividadeEntrevista.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
+
